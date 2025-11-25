1487c729ecd3c9cf4e95eac487445f3537831ed1
Jamie Steiner <jvsteiner@gmail.com>
2025-11-26 01:28:20 +0200

    bugfix for when mode is set on

--- frontend/src/extensions/Deletion.ts
+++ frontend/src/extensions/Deletion.ts
@@ -1,80 +1,84 @@
-import { Mark, mergeAttributes } from '@tiptap/core'
+import { Mark, mergeAttributes } from "@tiptap/core";
 
 export interface DeletionOptions {
-  HTMLAttributes: Record<string, unknown>
+  HTMLAttributes: Record<string, unknown>;
 }
 
-declare module '@tiptap/core' {
+declare module "@tiptap/core" {
   interface Commands<ReturnType> {
     deletion: {
-      setDeletion: (attributes: { id: string; author: string; date?: string }) => ReturnType
-      unsetDeletion: () => ReturnType
-      acceptDeletion: (id: string) => ReturnType
-      rejectDeletion: (id: string) => ReturnType
-    }
+      setDeletion: (attributes: {
+        id: string;
+        author: string;
+        date?: string;
+      }) => ReturnType;
+      unsetDeletion: () => ReturnType;
+      acceptDeletion: (id: string) => ReturnType;
+      rejectDeletion: (id: string) => ReturnType;
+    };
   }
 }
 
 export const Deletion = Mark.create<DeletionOptions>({
-  name: 'deletion',
+  name: "deletion",
 
   addOptions() {
     return {
       HTMLAttributes: {},
-    }
+    };
   },
 
   addAttributes() {
     return {
       id: {
         default: null,
-        parseHTML: element => element.getAttribute('data-deletion-id'),
-        renderHTML: attributes => {
-          if (!attributes.id) return {}
-          return { 'data-deletion-id': attributes.id }
+        parseHTML: (element) => element.getAttribute("data-deletion-id"),
+        renderHTML: (attributes) => {
+          if (!attributes.id) return {};
+          return { "data-deletion-id": attributes.id };
         },
       },
       author: {
         default: null,
-        parseHTML: element => element.getAttribute('data-author'),
-        renderHTML: attributes => {
-          if (!attributes.author) return {}
-          return { 'data-author': attributes.author }
+        parseHTML: (element) => element.getAttribute("data-author"),
+        renderHTML: (attributes) => {
+          if (!attributes.author) return {};
+          return { "data-author": attributes.author };
         },
       },
       date: {
         default: null,
-        parseHTML: element => element.getAttribute('data-date'),
-        renderHTML: attributes => {
-          if (!attributes.date) return {}
-          return { 'data-date': attributes.date }
+        parseHTML: (element) => element.getAttribute("data-date"),
+        renderHTML: (attributes) => {
+          if (!attributes.date) return {};
+          return { "data-date": attributes.date };
         },
       },
-    }
+    };
   },
 
   parseHTML() {
     return [
       {
-        tag: 'del[data-deletion-id]',
+        tag: "del[data-deletion-id]",
       },
       {
-        tag: 'span.deletion',
+        tag: "span.deletion",
       },
-    ]
+    ];
   },
 
   renderHTML({ HTMLAttributes }) {
     return [
-      'del',
+      "del",
       mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
-        class: 'deletion',
-        title: HTMLAttributes['data-author']
-          ? `Deleted by ${HTMLAttributes['data-author']}`
-          : 'Deletion',
+        class: "deletion",
+        title: HTMLAttributes["data-author"]
+          ? `Deleted by ${HTMLAttributes["data-author"]}`
+          : "Deletion",
       }),
       0,
-    ]
+    ];
   },
 
   addCommands() {
@@ -82,66 +86,72 @@ export const Deletion = Mark.create<DeletionOptions>({
       setDeletion:
         (attributes) =>
         ({ commands }) => {
-          return commands.setMark(this.name, attributes)
+          return commands.setMark(this.name, attributes);
         },
       unsetDeletion:
         () =>
         ({ commands }) => {
-          return commands.unsetMark(this.name)
+          return commands.unsetMark(this.name);
         },
       acceptDeletion:
         (id) =>
         ({ tr, state, dispatch }) => {
           // Accept deletion: remove the text entirely
-          const { doc } = state
-          const ranges: { from: number; to: number }[] = []
+          const { doc } = state;
+          const ranges: { from: number; to: number }[] = [];
+
+          // Mark this transaction so TrackChangesMode doesn't intercept it
+          tr.setMeta("acceptReject", true);
 
           doc.descendants((node, pos) => {
             if (node.isText) {
               const marks = node.marks.filter(
-                mark => mark.type.name === 'deletion' && mark.attrs.id === id
-              )
+                (mark) => mark.type.name === "deletion" && mark.attrs.id === id,
+              );
               if (marks.length > 0) {
-                ranges.push({ from: pos, to: pos + node.nodeSize })
+                ranges.push({ from: pos, to: pos + node.nodeSize });
               }
             }
-          })
+          });
 
           if (ranges.length > 0 && dispatch) {
             // Delete in reverse order to preserve positions
             ranges.reverse().forEach(({ from, to }) => {
-              tr.delete(from, to)
-            })
-            return true
+              tr.delete(from, to);
+            });
+            return true;
           }
 
-          return false
+          return false;
         },
       rejectDeletion:
         (id) =>
         ({ tr, state, dispatch }) => {
           // Reject deletion: remove the mark but keep the text
-          const { doc } = state
-          let found = false
+          const { doc } = state;
+          let found = false;
+
+          // Mark this transaction so TrackChangesMode doesn't intercept it
+          tr.setMeta("acceptReject", true);
 
           doc.descendants((node, pos) => {
             if (node.isText) {
               const marks = node.marks.filter(
-                mark => mark.type.name === 'deletion' && mark.attrs.id === id
-              )
+                (mark) => mark.type.name === "deletion" && mark.attrs.id === id,
+              );
               if (marks.length > 0) {
-                found = true
+                found = true;
                 if (dispatch) {
-                  tr.removeMark(pos, pos + node.nodeSize, this.type)
+                  tr.removeMark(pos, pos + node.nodeSize, this.type);
                 }
               }
             }
-          })
+          });
 
-          return found
+          return found;
         },
-    }
+    };
   },
-})
+});
 
-export default Deletion
+export default Deletion;
--- frontend/src/extensions/Insertion.ts
+++ frontend/src/extensions/Insertion.ts
@@ -1,80 +1,84 @@
-import { Mark, mergeAttributes } from '@tiptap/core'
+import { Mark, mergeAttributes } from "@tiptap/core";
 
 export interface InsertionOptions {
-  HTMLAttributes: Record<string, unknown>
+  HTMLAttributes: Record<string, unknown>;
 }
 
-declare module '@tiptap/core' {
+declare module "@tiptap/core" {
   interface Commands<ReturnType> {
     insertion: {
-      setInsertion: (attributes: { id: string; author: string; date?: string }) => ReturnType
-      unsetInsertion: () => ReturnType
-      acceptInsertion: (id: string) => ReturnType
-      rejectInsertion: (id: string) => ReturnType
-    }
+      setInsertion: (attributes: {
+        id: string;
+        author: string;
+        date?: string;
+      }) => ReturnType;
+      unsetInsertion: () => ReturnType;
+      acceptInsertion: (id: string) => ReturnType;
+      rejectInsertion: (id: string) => ReturnType;
+    };
   }
 }
 
 export const Insertion = Mark.create<InsertionOptions>({
-  name: 'insertion',
+  name: "insertion",
 
   addOptions() {
     return {
       HTMLAttributes: {},
-    }
+    };
   },
 
   addAttributes() {
     return {
       id: {
         default: null,
-        parseHTML: element => element.getAttribute('data-insertion-id'),
-        renderHTML: attributes => {
-          if (!attributes.id) return {}
-          return { 'data-insertion-id': attributes.id }
+        parseHTML: (element) => element.getAttribute("data-insertion-id"),
+        renderHTML: (attributes) => {
+          if (!attributes.id) return {};
+          return { "data-insertion-id": attributes.id };
         },
       },
       author: {
         default: null,
-        parseHTML: element => element.getAttribute('data-author'),
-        renderHTML: attributes => {
-          if (!attributes.author) return {}
-          return { 'data-author': attributes.author }
+        parseHTML: (element) => element.getAttribute("data-author"),
+        renderHTML: (attributes) => {
+          if (!attributes.author) return {};
+          return { "data-author": attributes.author };
         },
       },
       date: {
         default: null,
-        parseHTML: element => element.getAttribute('data-date'),
-        renderHTML: attributes => {
-          if (!attributes.date) return {}
-          return { 'data-date': attributes.date }
+        parseHTML: (element) => element.getAttribute("data-date"),
+        renderHTML: (attributes) => {
+          if (!attributes.date) return {};
+          return { "data-date": attributes.date };
         },
       },
-    }
+    };
   },
 
   parseHTML() {
     return [
       {
-        tag: 'ins[data-insertion-id]',
+        tag: "ins[data-insertion-id]",
       },
       {
-        tag: 'span.insertion',
+        tag: "span.insertion",
       },
-    ]
+    ];
   },
 
   renderHTML({ HTMLAttributes }) {
     return [
-      'ins',
+      "ins",
       mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
-        class: 'insertion',
-        title: HTMLAttributes['data-author']
-          ? `Inserted by ${HTMLAttributes['data-author']}`
-          : 'Insertion',
+        class: "insertion",
+        title: HTMLAttributes["data-author"]
+          ? `Inserted by ${HTMLAttributes["data-author"]}`
+          : "Insertion",
       }),
       0,
-    ]
+    ];
   },
 
   addCommands() {
@@ -82,66 +86,74 @@ export const Insertion = Mark.create<InsertionOptions>({
       setInsertion:
         (attributes) =>
         ({ commands }) => {
-          return commands.setMark(this.name, attributes)
+          return commands.setMark(this.name, attributes);
         },
       unsetInsertion:
         () =>
         ({ commands }) => {
-          return commands.unsetMark(this.name)
+          return commands.unsetMark(this.name);
         },
       acceptInsertion:
         (id) =>
         ({ tr, state, dispatch }) => {
           // Accept insertion: remove the mark but keep the text
-          const { doc } = state
-          let found = false
+          const { doc } = state;
+          let found = false;
+
+          // Mark this transaction so TrackChangesMode doesn't intercept it
+          tr.setMeta("acceptReject", true);
 
           doc.descendants((node, pos) => {
             if (node.isText) {
               const marks = node.marks.filter(
-                mark => mark.type.name === 'insertion' && mark.attrs.id === id
-              )
+                (mark) =>
+                  mark.type.name === "insertion" && mark.attrs.id === id,
+              );
               if (marks.length > 0) {
-                found = true
+                found = true;
                 if (dispatch) {
-                  tr.removeMark(pos, pos + node.nodeSize, this.type)
+                  tr.removeMark(pos, pos + node.nodeSize, this.type);
                 }
               }
             }
-          })
+          });
 
-          return found
+          return found;
         },
       rejectInsertion:
         (id) =>
         ({ tr, state, dispatch }) => {
           // Reject insertion: remove the text entirely
-          const { doc } = state
-          const ranges: { from: number; to: number }[] = []
+          const { doc } = state;
+          const ranges: { from: number; to: number }[] = [];
+
+          // Mark this transaction so TrackChangesMode doesn't intercept it
+          tr.setMeta("acceptReject", true);
 
           doc.descendants((node, pos) => {
             if (node.isText) {
               const marks = node.marks.filter(
-                mark => mark.type.name === 'insertion' && mark.attrs.id === id
-              )
+                (mark) =>
+                  mark.type.name === "insertion" && mark.attrs.id === id,
+              );
               if (marks.length > 0) {
-                ranges.push({ from: pos, to: pos + node.nodeSize })
+                ranges.push({ from: pos, to: pos + node.nodeSize });
               }
             }
-          })
+          });
 
           if (ranges.length > 0 && dispatch) {
             // Delete in reverse order to preserve positions
             ranges.reverse().forEach(({ from, to }) => {
-              tr.delete(from, to)
-            })
-            return true
+              tr.delete(from, to);
+            });
+            return true;
           }
 
-          return false
+          return false;
         },
-    }
+    };
   },
-})
+});
 
-export default Insertion
+export default Insertion;
